b5b8e35d4b85952429f2443495e8d04fbe726e0b
moses-ramoeletsi <mraomoeletsi@gmail.com>
2023-09-17 20:13:14 +0200

    Modified files

--- excel_dashboard_app.py
+++ excel_dashboard_app.py
@@ -13,8 +13,9 @@ st.title(':chart_with_upwards_trend: Welcome To SLA Cost Analysis Dashboard')
 uploaded_files = st.file_uploader("Upload Excel files (SLA Data and 'FNB_CARD_DRIVERS.xlsx')", type=["xlsx"], accept_multiple_files=True)
 
 # Separate SLA data and Card Drivers data based on file names
-uploaded_sla_files = [file for file in uploaded_files if "FNB_CARD_DRIVERS" not in file.name]
+uploaded_sla_files = [file for file in uploaded_files if "FNB_CARD_DRIVERS" not in file.name and "GROUP_CRIME_DRIVERS" not in file.name]
 uploaded_cards_file = [file for file in uploaded_files if "FNB_CARD_DRIVERS" in file.name]
+uploaded_group_crime_file = [file for file in uploaded_files if "GROUP_CRIME_DRIVERS" in file.name]
 
 if uploaded_sla_files:
     # Combine SLA data from multiple uploaded files into a single DataFrame
@@ -22,6 +23,7 @@ if uploaded_sla_files:
 
     # Load Card Drivers data from the 'FNB_CARD_DRIVERS.xlsx' file if available
     data1 = pd.read_excel(uploaded_cards_file[0]) if uploaded_cards_file else None
+    data2 = pd.read_excel(uploaded_group_crime_file[0]) if uploaded_group_crime_file else None
 
     st.sidebar.subheader("Risk Department")
     st.subheader('Data from Excel file')
@@ -29,6 +31,7 @@ if uploaded_sla_files:
     # Create a multiselect widget for selecting columns in the sidebar
     available_SLA_columns = data.columns.tolist()  # Get a list of available column names
     available_CardsDrivers_columns = data1.columns.tolist() if data1 is not None else []
+    available_group_crime_columns = data2.columns.tolist() if data2 is not None else []
 
     selected_SLA_columns = st.sidebar.multiselect('Select Cost Category:', available_SLA_columns)
 
@@ -109,6 +112,47 @@ if uploaded_sla_files:
                     
                     # Display the bar chart
                     st.plotly_chart(fig_card_drivers, use_container_width=True)
+
+            # Display specific content if "Group Crime" is selected
+            if "Group Crime" in selected_SLA_columns:
+                st.write(f'Inside Group Crime section')
+                with col2:
+                    st.write('Bar Chart for Group Crime Drivers Data:')
+                    
+                    if data2 is not None:  # Check if Group Crime data is available
+                        # Create a function to filter data based on selected date range
+                        def filter_data_by_date(selected_start_date, selected_end_date):
+                            filtered_data2 = data2[(data2["Date"] >= selected_start_date) & (data2["Date"] <= selected_end_date)]
+                            return filtered_data2
+                        
+                        # Get the selected start and end dates
+                        selected_start_date = date1
+                        selected_end_date = date2
+                        
+                        # Filter data based on selected date range
+                        filtered_data2 = filter_data_by_date(selected_start_date, selected_end_date)
+                        
+                        if not filtered_data2.empty:
+                            # Create the bar chart
+                            fig_group_crime = go.Figure()
+                            for col in available_group_crime_columns:
+                                if col != "Date":  # Exclude the "Date" column
+                                    fig_group_crime.add_trace(go.Bar(x=filtered_data2['Date'], y=filtered_data2[col], name=col))
+                            
+                            # Customize the layout of the bar chart
+                            fig_group_crime.update_layout(
+                                xaxis_title="Date",
+                                yaxis_title="Value",
+                                barmode='group',  # You can change the barmode as needed
+                            )
+                            
+                            # Display the bar chart
+                            st.plotly_chart(fig_group_crime, use_container_width=True)
+                        else:
+                            st.warning("No data available for the selected date range.")
+                    else:
+                        st.warning("Group Crime Drivers data not available.")
+
 
         # Calculate and display statistical descriptions for selected columns
         if selected_SLA_columns:
